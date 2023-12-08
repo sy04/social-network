f236db25e174a99e218bcbcdeb1318eb28224002
sy04 <sy.fitrawan@gmail.com>
2023-12-09 06:45:18 +0700

    Revert "🔥 clear cache"
    This reverts commit 1f2b8c4b02ef050d50f7101546531d05cc4f3993.

---
+++ social_network_backend/post/forms.py
@@ -0,0 +1,12 @@
+from django.forms import ModelForm
+from .models import Post, PostAttachment
+
+class PostForm(ModelForm):
+  class Meta:
+    model = Post
+    fields = ('body', 'is_private')
+
+class AttachmentForm(ModelForm):
+  class Meta:
+    model = PostAttachment
+    fields = ('image',)
