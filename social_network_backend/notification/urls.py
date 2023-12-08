1f2b8c4b02ef050d50f7101546531d05cc4f3993
sy04 <sy.fitrawan@gmail.com>
2023-12-09 06:39:22 +0700

    🔥 clear cache

--- social_network_backend/notification/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import api
-
-urlpatterns = [
-  path('', api.notifications, name='notifications'),
-  path('read/<uuid:pk>/', api.read_notification, name='read_notification')
-]
